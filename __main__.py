b4e2e99ee90a19edb44d4a3ac37f40bb4dd048bf
NikolaVukashinovich <nikola.vukasinovic@spotsie.io>
2023-11-29 14:55:33 +0100

    added kubeconfig export

--- __main__.py
+++ __main__.py
@@ -6,10 +6,12 @@ import pulumi_azure as azure
 # Retrieve Azure configuration
 config = Config()
 location = config.get("location") or "West Europe"
-res_group = config.get("resource_group") or "test_group"
+res_group = config.get("resource_group") or "test-group"
 vm_size = config.get("vm_size") or "Standard_D2_v2"
-node_count = config.get("node_count") or 1
+node_count = config.get("node_count") or 2
 env = config.get("environment") or "Development"
+dns_prefix = config.get("dns_prefix") or "testAKSCluster"
+dns_prefix_private_cluster = config.get("dns_prefix_private_cluster") or None
 
 # Create an Azure Resource Group
 resource_group = azure_native.resources.ResourceGroup("resourceGroup",
@@ -20,7 +22,8 @@ resource_group = azure_native.resources.ResourceGroup("resourceGroup",
 aks_cluster = azure.containerservice.KubernetesCluster("testAKSCluster",
     location=resource_group.location,
     resource_group_name=resource_group.name,
-    #dns_prefix="exampleaks1", # Let Azure set default
+    dns_prefix=dns_prefix,
+    dns_prefix_private_cluster=dns_prefix_private_cluster,
     default_node_pool=azure.containerservice.KubernetesClusterDefaultNodePoolArgs(
         name="default",
         node_count=node_count,
